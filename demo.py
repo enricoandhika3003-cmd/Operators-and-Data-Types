2393b543b368d0429d93285ea8ebd11364dadfda
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-12-19 18:57:41 +0700

    done

---
+++ demo.py
@@ -0,0 +1,39 @@
+name = "Enrico"
+age = 14
+weight = 54.1
+student = True
+
+print(str(age))
+convertNum = int(weight)
+print(convertNum)
+
+num1 = 6
+num2 = 7
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1**num2)
+print(num2%num1)
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
+
+firstName = "Enrico"
+lastName = "Efendi"
+fullName = firstName + " " + lastName
+multiplyName = firstName*3
+
+print(firstName)
+print(lastName)
+print(fullName)
+print(multiplyName)
+
+middleName = "Andhika"
+print(len(middleName))
+print(middleName[0:1])
+print(middleName[6:7])
