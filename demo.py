c8c9659c3345b15a632e4c0bbafa9b8a371f3387
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-12-26 17:44:40 +0700

    done 3

--- demo.py
+++ demo.py
@@ -38,3 +38,19 @@ print(len(middleName))
 print(middleName[0:1])
 print(middleName[6:7])
 print(middleName[0:3])
+
+x = input("Enter X value = ")
+y = input("Enter Y value = ")
+z = input("Enter Z value = ")
+
+temp = x
+temp2 = y
+x = z
+y = temp
+z = temp2
+
+print("In ascending order,")
+print("X is ", x)
+print("Y is ", y)
+print("Z is", z)
+
